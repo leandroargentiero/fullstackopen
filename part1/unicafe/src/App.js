6eaa0cf4fcc3d5565725178f379c654a43b772a5
Leandro Argentiero <argentiero.leo@gmail.com>
2022-01-04 14:04:24 +0100

    Finished step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,39 @@
+import React, { useState } from 'react';
+
 function App() {
-  return <div>code here.</div>;
+  //save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = (value) => {
+    console.log('---clicked ' + value);
+
+    switch (value) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+    }
+  };
+
+  return (
+    <>
+      <h2>Give feedback</h2>
+      <button onClick={() => handleClick('good')}>Good</button>
+      <button onClick={() => handleClick('neutral')}>Neutral</button>
+      <button onClick={() => handleClick('bad')}>Bad</button>
+      <h3>Statistics</h3>
+      <p>Good:{good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad:{bad}</p>
+    </>
+  );
 }
 
 export default App;
