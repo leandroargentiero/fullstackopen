9303c2ac1802186775d9b69c37018c2bfdb69b38
Leandro Argentiero <argentiero.leo@gmail.com>
2022-01-04 15:24:25 +0100

    Finished step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,37 @@
 import React, { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+
+  const calcAverage = () => {
+    const avgGood = good * 1;
+    const avgNeutral = neutral * 0;
+    const avgBad = bad * -1;
+
+    return total > 0 ? (avgGood + avgNeutral + avgBad) / total : 0;
+  };
+
+  const calcPositive = () => (total > 0 ? (good / total) * 100 : 0);
+
+  return (
+    <>
+      <h3>Statistics</h3>
+      {total > 0 ? (
+        <>
+          <p>Good:{good}</p>
+          <p>Neutral:{neutral}</p>
+          <p>Bad:{bad}</p>
+          <p>All:{good + neutral + bad}</p>
+          <p>Average: {calcAverage()}</p>
+          <p>Positive: {calcPositive()}%</p>
+        </>
+      ) : (
+        <p>No feedback given.</p>
+      )}
+    </>
+  );
+};
+
 function App() {
   //save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -22,34 +54,13 @@ function App() {
     }
   };
 
-  const calcAverage = () => {
-    const avgGood = good * 1;
-    const avgNeutral = neutral * 0;
-    const avgBad = bad * -1;
-    const total = good + neutral + bad;
-
-    return (avgGood + avgNeutral + avgBad) / total;
-  };
-
-  const calcPositive = () => {
-    const total = good + neutral + bad;
-
-    return (good / total) * 100;
-  };
-
   return (
     <>
       <h2>Give feedback</h2>
       <button onClick={() => handleClick('good')}>Good</button>
       <button onClick={() => handleClick('neutral')}>Neutral</button>
       <button onClick={() => handleClick('bad')}>Bad</button>
-      <h3>Statistics</h3>
-      <p>Good:{good}</p>
-      <p>Neutral:{neutral}</p>
-      <p>Bad:{bad}</p>
-      <p>All:{good + neutral + bad}</p>
-      <p>Average: {calcAverage()}</p>
-      <p>Positive: {calcPositive()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 }
