e43b1eb07db77ca050cd0eb7c962bbaa266cb918
Leandro Argentiero <argentiero.leo@gmail.com>
2022-01-14 20:35:14 +0100

    Finished 3.5 - step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -24,6 +24,14 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+// express json-parser for receiving data
+app.use(express.json());
+
 // GET root directory
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook App!</h1>');
@@ -34,7 +42,7 @@ app.get('/api/persons', (request, response) => {
   response.send(persons);
 });
 
-// GET specific person
+// GET person
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
@@ -44,6 +52,36 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+// POST new person
+// uses -> express json-parser for receiving data
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+// DELETE person
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 // GET info
@@ -56,15 +94,6 @@ app.get('/info', (request, response) => {
   `;
 
   response.send(html);
-});
-
-// DELETE Specific Person
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  console.log(id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
 });
 
 // SERVER DESIGNATION
