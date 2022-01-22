886d7746e560af5415c4a8b802d80240970ed284
Leandro Argentiero <argentiero.leo@gmail.com>
2022-01-22 14:33:53 +0100

    Implemented eslint + prettier

--- part4/blog-list/.eslintrc.js
+++ part4/blog-list/.eslintrc.js
@@ -1,34 +1,9 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
-  plugins: ['react'],
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 0,
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'no-underscore-dangle': 'allow',
   },
-}
+};
--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,26 +1,31 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./utils/config');
 
-logger.info('connecting to', config.MONGODB_URI)
+const app = express();
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 
-mongoose.connect(config.MONGODB_URI)
-  .then( () => {console.log('connected to MongoDB')})
-  .catch(error => console.log('error connecting to MongoDB: ', error.message))
+logger.info('connecting to', config.MONGODB_URI);
 
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) =>
+    console.log('error connecting to MongoDB: ', error.message)
+  );
 
-app.use(cors())
+app.use(cors());
 // app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
 
-app.use(middleware.unknownEndpoint)
+app.use(middleware.unknownEndpoint);
 
-module.exports = app
+module.exports = app;
