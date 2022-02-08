2ee682f375eecd90cd0cc5f37d9725e960bbab57
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-08 16:20:05 +0100

    Finished 4.15

---
+++ part4/blog-list/models/user.js
@@ -0,0 +1,23 @@
+/* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["returnedObject"] }] */
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id", "__v"] }] */
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
