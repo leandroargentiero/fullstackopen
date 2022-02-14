0cc30a2d8a57a816f80fb40329e742efd1d88e83
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-14 14:43:58 +0100

    Finished 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,17 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 function App() {
   const [blogs, setBlogs] = useState([]);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((allBlogs) => setBlogs(allBlogs));
   }, []);
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const loggedInUser = await loginService({
+        username,
+        password,
+      });
+
+      setUser(loggedInUser);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <div>
+        <label htmlFor="username">
+          username{' '}
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="password">
+          password{' '}
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  if (user === null) {
+    return loginForm();
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{`${user.name} logged in`}</p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
