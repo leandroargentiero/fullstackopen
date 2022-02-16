9888a1732d8131fa6463646d64971e51cbbfc25e
Leandro Argentiero <argentiero.leo@gmail.com>
2022-02-16 12:41:29 +0100

    Finished 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,16 +1,48 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
 const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
+  const detailsVisibility = { display: detailsVisible ? '' : 'none' };
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button type="button" onClick={toggleVisibility}>
+          {detailsVisible ? 'hide' : 'show'}
+        </button>
+      </div>
+      <div style={detailsVisibility}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button type="button">like</button>
+        </div>
+      </div>
     </div>
   );
 };
 
 Blog.propTypes = {
-  blog: PropTypes.shape({ title: PropTypes.string, author: PropTypes.string })
-    .isRequired,
+  blog: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+    url: PropTypes.string,
+    likes: PropTypes.number,
+  }).isRequired,
 };
 
 export default Blog;
